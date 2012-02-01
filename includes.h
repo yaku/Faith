cbf265c77e74ef47409db0e4469d9ff0bc28b6ae
Denis Yakunchikov <toi.yaku@gmail.com>
2012-02-01 19:55:13 +0400

    Fixed implicid declaration of pread function

--- includes.h
+++ includes.h
@@ -7,17 +7,24 @@
  *
  */
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include <time.h>
 
+#define _XOPEN_SOURCE 500
+#include <unistd.h>
+extern ssize_t pread(int, void *, size_t, off_t);
+
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <linux/fs.h>
-#include <unistd.h>
+
+
+
 #include <errno.h>
 
